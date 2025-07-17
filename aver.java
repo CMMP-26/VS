d0323d1e04446e04f3f87e4718800fccfa098fd3
CMMP <loc6785@gmail.com>
2025-07-17 00:10:32 -0600

    Otra prueba

--- aver.java
+++ aver.java
@@ -1,5 +1,6 @@
 public class aver {
     public static void main(String[] args) {
         System.out.println("Hello linux and welcome to my laptop");
+        System.out.println("Prueba de git y github");
     }
 }
